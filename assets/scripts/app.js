163fdac26249b961faed19817998648eebcf5bc8
aleksandGG <aleksandrgaranin89@gmail.com>
2020-11-21 13:20:18 -0800

    added 2 classes Shop and ShoppingCart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,19 @@ class Product {
 
 // console.log(new Product())
 
+class ShoppingCart{
+    items = [];
+
+    render() {
+        const cartEl = document.createElement('section')
+        cartEl.innerHTML = `
+            <h2>Total: \$${0}</h2>
+            <button>Order Now!</button>
+        `;
+        cartEl.className = "cart";
+        return cartEl;
+    }
+}
 
 class ProductItem {
     constructor(product) {
@@ -65,7 +78,6 @@ class ProductList {
     constructor() { }
 
     render() {
-        const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
@@ -73,9 +85,23 @@ class ProductList {
             const prodEl = productItem.render()
             prodList.append(prodEl)
         }
-        renderHook.append(prodList)
+        return prodList;
     }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+    render() {
+        const renderHook = document.getElementById('app');
+
+        const cart = new ShoppingCart();
+        const cartEl = cart.render()
+        const productList = new ProductList();
+        const prodListEl = productList.render()
+
+        renderHook.append(cartEl)
+        renderHook.append(prodListEl)
+    }
+}
+
+const shop = new Shop()
+shop.render()
