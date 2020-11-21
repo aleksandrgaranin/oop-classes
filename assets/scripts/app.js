a1709d213c15e732cd55ea2ee37bb4a6eb68be1b
aleksandGG <aleksandrgaranin89@gmail.com>
2020-11-21 14:26:17 -0800

    GETters & SETters added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,13 +15,27 @@ class Product {
 
 // console.log(new Product())
 
-class ShoppingCart{
+class ShoppingCart {
     items = [];
-    
-    addProduct(product){
-        this.items.push(product);
-        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`
+    set cartItems(value) {
+        this.items = value
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
 
+        console.log(this.items);
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce((prevValue, curItem) => {
+            return prevValue + curItem.price
+        }, 0);
+        return sum;
+    }
+    
+
+    addProduct(product) {
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
     }
 
     render() {
@@ -113,7 +127,7 @@ class Shop {
 
 class App {
     static cart;
-    
+
     static init() {
         const shop = new Shop();
         shop.render()
