07ac8295df4b04a1566b2d1548ab2afb499237cf
ALEKS G <aleksandrgaranin89@gmail.com>
2020-11-22 01:25:21 -0600

    swiched from public property of 'products' to private '#products

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -128,15 +128,16 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component{
-    products = [];
+    #products = [];
 
     constructor(renderHookId) {
-        super(renderHookId);
+        super(renderHookId, false);
+        this.render()
         this.fetchProducts();
     }
 
     fetchProducts() {
-        this.products = [
+        this.#products = [
             new Product(
                 'A pillow',
                 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs.yimg.com%2Faah%2Fyhst-72531153481428%2Fpillowtex-reg-luxury-down-and-feather-pillow-40.gif&f=1&nofb=1',
@@ -153,7 +154,7 @@ class ProductList extends Component{
     }
 
     renderProducts() {
-        for (const prod of this.products) {
+        for (const prod of this.#products) {
             new ProductItem(prod, 'prod-list')      
         }
     }
@@ -161,7 +162,7 @@ class ProductList extends Component{
     render() {
         const prodList = this.createRootElement('ul', 'product-list',
          [new ElementAttribute('id', 'prod-list')]);
-        if (this.products && this.products.length > 0) {
+        if (this.#products && this.#products.length > 0) {
             this.renderProducts();
         }
     }
