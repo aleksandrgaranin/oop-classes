6889945b82622cc883d1fc2d0e7221bb1e88f025
ALEKS G <aleksandrgaranin89@gmail.com>
2020-12-01 23:32:33 -0600

    custom poitioning of Tooltip element

--- practice-oop/assets/scripts/app.js
+++ practice-oop/assets/scripts/app.js
@@ -52,9 +52,23 @@ class Tooltip extends Componet {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
         tooltipElement.textContent = this.text;
+
+        const hostElPosLeft = this.hostElement.offsetLeft;
+        const hostElPosTop = this.hostElement.offsetTop;
+        const hostElHight = this.hostElement.clientHeight;
+        const parantElementScrolling = this.hostElement.parentElement.scrollTop; // for solving scrolling issue
+
+
+        const x = hostElPosLeft + 20;
+        const y = hostElPosTop+ hostElHight - parantElementScrolling - 10;
+
+        tooltipElement.style.position = 'absolute'// requierd for change position of element
+        tooltipElement.style.left = x + 'px';
+        tooltipElement.style.top = y + 'px';
+
         tooltipElement.addEventListener('click', this.closeTooltip);
         this.element = tooltipElement;
-        console.log(this.hostElement.getBoundingClientRect())
+        // console.log(this.hostElement.getBoundingClientRect())
     }
 
 
